951a3a5b9b25f7cfe33bf90e0241a60f3a8dc6b9
Ivan-Alejo <31460009+Ivan-Alejo@users.noreply.github.com>
2017-10-02 15:41:52 -0500

    intento uno utilizando la forma iterativa

---
+++ src/Arreglo.java
@@ -0,0 +1,12 @@
+public class Arreglo {
+    public static void main(String[] args){
+
+
+        int x[]={2,5,3,4,3};
+
+
+       if(x[0]<x[1]){System.out.println(x[0]);}
+       else{System.out.println(x[1]);}
+
+    }
+}
