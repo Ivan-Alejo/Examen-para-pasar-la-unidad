edcac6d2e783412e1eb66f36b22d80c80a64c193
Ivan-Alejo <31460009+Ivan-Alejo@users.noreply.github.com>
2017-10-02 21:50:48 -0500

    version 3.0 usando otra forma

--- src/Arreglo.java
+++ src/Arreglo.java
@@ -1,12 +1,87 @@
 public class Arreglo {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
 
-        int x[]={2,5,3,4,3};
+
+        int A = -900;
+
+        int B = 3;
+
+        int C = -90;
+
+        int D = -5;
+
+        int E = 100;
 
 
-       if(x[0]<x[1]){System.out.println(x[0]);}
-       else{System.out.println(x[1]);}
+
+
+        if (A < B) { A += B;  B = A - B;  A -= B; }
+        else{}
+        if (A < C) { A += C;  C = A -C;  A -= C; }
+        else{}
+        if (A < D) { A += D;  D = A - D;  A -= D; }
+        else{}
+        if (A < E) { A += E;  E = A - E;  A -= E; }
+        else{}
+
+
+
+        if (B < C) { B += C;  C = B - C;  B -= C; }
+        else{}
+        if (B < D) { B += D;  D= B - D;  B -= D; }
+        else{}
+        if (B < E) { B += E;  E = B - E;  B -= E; }
+        else{}
+
+
+
+        if (C< D) { C += D;  D = C - D;  C -= D; }
+        else {}
+        if (C < E) {C+= E;  E = C - E;  C -= E; }
+        else {}
+
+
+
+        if (D < E) { D += E;  E = D - E;  D -= E; }
+        else{}
+
+
+        System.out.println(E);
+        System.out.println(D);
+        System.out.println(C);
+        System.out.println(B);
+        System.out.println(A);
+
+
+
+
+
+
 
     }
-}
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
