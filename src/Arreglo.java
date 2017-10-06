c975a374970f8c556988375d42b9b1bb7b097336
Ivan-Alejo <31460009+Ivan-Alejo@users.noreply.github.com>
2017-10-05 21:46:17 -0500

    ya en termine 2 de tres formas de resolver este arreglo

--- src/Arreglo.java
+++ src/Arreglo.java
@@ -3,19 +3,58 @@ public class Arreglo {
     public static void main(String[] args) {
 
 
-        Arreglo x = new Arreglo();
-        x.cualquiera( new int []{5,5,9,6,3, 5,5,6,3,4,-500});
+        int[] b={86,-96,300,0,-4,};
+        Arreglo y=new Arreglo();
+        y.complicado(b);
+
+        System.out.println();
+
+       int[] a={2,5,6,-78,9,0,66,-5,63,1000};
+       Arreglo x=new Arreglo();
+       x.cualquiera(a);
+
+       System.out.println();
 
 
-       for (int i = 0; i <0 ; i++) {
-           System.out.println();
-       }
+        int[] c={89,56,-5,6,0,};
+        Arreglo z =new Arreglo();
+        z.recurisividad(c, c.length);
 
 
     }
 
+    public void complicado(int[] n){
 
-    public static int[] cualquiera(int []n){
+        if (n[0] < n[1]) { n[0] += n[1];  n[1] = n[0] - n[1];  n[0] -= n[1]; }
+        else{}
+        if (n[0] < n[2]) { n[0] += n[2];  n[2] = n[0] -n[2];  n[0] -= n[2]; }
+        else{}
+        if (n[0] < n[3]) { n[0] += n[3];  n[3] = n[0] - n[3];  n[0] -= n[3]; }
+        else{}
+        if (n[0] < n[4]) { n[0] += n[4];  n[4] = n[0] - n[4];  n[0] -= n[4]; }
+        else{}
+
+        if (n[1] < n[2]) { n[1] += n[2];  n[2] = n[1] - n[2];  n[1] -= n[2]; }
+        else{}
+        if (n[1] < n[3]) { n[1] += n[3];  n[3]= n[1] - n[3];  n[1] -= n[3]; }
+        else{}
+        if (n[1] < n[4]) { n[1] += n[4];  n[4] = n[1] - n[4];  n[1]-= n[4]; }
+        else{}
+
+        if (n[2]< n[3]) { n[2] += n[3];  n[3] = n[2] - n[3];  n[2] -= n[3]; }
+        else {}
+        if (n[2] < n[4]) {n[2]+= n[4];  n[4] = n[2] - n[4];  n[2] -= n[4]; }
+        else {}
+
+        if (n[3] < n[4]) { n[3] += n[4];  n[4] = n[3] - n[4];  n[3] -= n[4]; }
+        else{}
+
+        System.out.print("["+n[4]+"]"+" "+"["+n[3]+"]"+" "+"["+n[2]+"]"+" "+"["+n[1]+"]"+" "+"["+n[0]+"]"+" ");
+
+
+    }
+
+    public void cualquiera(int[] n){
         int b;
 
 
@@ -24,14 +63,33 @@ public class Arreglo {
                 if (n[i] < n[j]) {
                     b = n[i];
                     n[i] = n[j];
-                    n[j] = b;
-                }
-            }
-        }
+                    n[j] = b; } } }
 
+       for(int i=0; i<n.length; i++){
+            System.out.print("["+n[i]+"]"+" ");
+       }
 
-        return n;
     }
+
+   public void recurisividad(int[] n, ){
+        int aux;
+        if(n<1){
+            for (int j = 0; j < n.length; j++) {
+                if (n[j] < n[j+1]) {
+                    aux = n[j];
+                    n[j] = n[j+1];
+                    n[j+1] = aux; } }
+
+                    recurisividad(n,a-1);
+
+
+            }
+
+
+
+    }
+
+
 
 }
 
