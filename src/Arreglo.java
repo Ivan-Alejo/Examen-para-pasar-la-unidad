818705c9cf5ec95576cb256b88c04daf46cab105
Ivan-Alejo <31460009+Ivan-Alejo@users.noreply.github.com>
2017-10-06 20:48:51 -0500

    este es la el final de mi examen listo para entregar, porfin soy libre :)

--- src/Arreglo.java
+++ src/Arreglo.java
@@ -3,13 +3,13 @@ public class Arreglo {
     public static void main(String[] args) {
 
 
-        int[] b={86,-96,300,0,-4,};
+        int[] b={86,-96,30,0,-4,};
         Arreglo y=new Arreglo();
         y.complicado(b);
 
         System.out.println();
 
-       int[] a={2,5,6,-78,9,0,66,-5,63,1000};
+       int[] a={2,5,6,-78,9,0,66,-5,63,1000,-88888,1};
        Arreglo x=new Arreglo();
        x.cualquiera(a);
 
@@ -25,6 +25,8 @@ public class Arreglo {
 
     public void complicado(int[] n){
 
+        //este bloque compara los 5 elemementos del array y si se cumple la condicion le asigna el valor, en este caso en el lugar n[0] se le asigna el valor ma chico
+        // y en el lugar n[4] si se cumple la condicion se  le pone el valor mas alto
         if (n[0] < n[1]) { n[0] += n[1];  n[1] = n[0] - n[1];  n[0] -= n[1]; }
         else{}
         if (n[0] < n[2]) { n[0] += n[2];  n[2] = n[0] -n[2];  n[0] -= n[2]; }
@@ -55,9 +57,8 @@ public class Arreglo {
     }
 
     public void cualquiera(int[] n){
-        int b;
-
-
+       //aqui utilize dos ciclos if para comparar los elementso del array que ambos empiezan en 0 y terminan dependiendo los elementos del array con .length; y aumentan de uno a la vez
+        int b;//utilice este variable para almacenar n[i] y despues n[j]
         for (int i = 0; i < n.length; i++) {
             for (int j = 0; j < n.length; j++) {
                 if (n[i] < n[j]) {
@@ -70,7 +71,7 @@ public class Arreglo {
        }
 
     }
-
+// no puede decir la recursividad profe :(
    /*public void recurisividad(int[] n){
         int aux;
         if(n<1){
